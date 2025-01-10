293c75960fc045da069ce24454ef0c609a28b04f
Lindows <sangeoul@naver.com>
2025-01-10 21:11:10 +0000

    asdf

--- app/appstart.sh
+++ app/appstart.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
 # Start cron in the background
-# python3 /app/crawler.py
+# python3 crawler.py
 
 cron &
 echo "Cron daemon started at $(date)"
-
-#flask run --host=0.0.0.0 --port=8000 
-
-
 
 # Tail the correct log file for cron
 tail -f /var/log/crawler.log 
