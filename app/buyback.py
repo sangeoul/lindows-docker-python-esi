3444cd2378d22a216e6ff6a14922e8974a2806e7
Lindows <sangeoul@naver.com>
2025-01-12 19:35:43 +0000

    asdf

--- app/buyback.py
+++ app/buyback.py
@@ -139,7 +139,6 @@ def create_buyback_item(input_name, input_amount, language,whitelist=None):
         if(get_reprocessing_or_not(input_id, group_id, whitelist)):
 
             
-            print(f"!!DEBUG F: {[min_br,default_br,max_br]}",flush=True)
             # Populate outputs
             conn = connect_to_db()
             cursor = conn.cursor()
@@ -245,21 +244,17 @@ def get_reprocessing_or_not(input_id, group_id, whitelist):
 
 def get_buyback_rate(input_id, group_id, whitelist):
 
-    print(f"!!DEBUG : {input_id},{group_id}",flush=True)
     # Check in type_id list first (priority)
     for item in whitelist["type_id"]:
         if item["id"] == input_id and "buyback_rate" in item:
-            print(f"!!DEBUG 1 : {item['buyback_rate']}",flush=True)
             return item["buyback_rate"][0],item["buyback_rate"][1],item["buyback_rate"][2]
     
     # Check in group_id list if not found in type_id
     for group in whitelist["group_id"]:
         if group["id"] == group_id and "buyback_rate" in group:
-            print(f"!!DEBUG 2 : {item['buyback_rate']}",flush=True)
             return group["buyback_rate"][0],group["buyback_rate"][1],group["buyback_rate"][2]
 
     # Default to False if not found or no reprocessing value
-    print(f"!!DEBUG 3 : {[MINIMUM_BUYBACK_RATE, DEFAULT_BUYBACK_RATE, MAX_BUYBACK_RATE]}",flush=True)
     return MINIMUM_BUYBACK_RATE, DEFAULT_BUYBACK_RATE, MAX_BUYBACK_RATE
 
 def get_refining_rate_for_item(group_id,whitelist):
