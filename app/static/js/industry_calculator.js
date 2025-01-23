99d7531da4c3df910d9b0874bed366929030aa92
Lindows <sangeoul@naver.com>
2025-01-23 16:34:29 +0000

    asdf

--- app/static/js/industry_calculator.js
+++ app/static/js/industry_calculator.js
@@ -279,8 +279,9 @@ async function calcStructureBonus(industry_type) {
     else if (systemSecurity > 0) SYSTEM_BONUS_MULTIPLIER = 1.9;
     else SYSTEM_BONUS_MULTIPLIER = 2.1;
 
-    const structure_bonus = ((1 - (1 - (currentRigOption.rig_bonus * SYSTEM_BONUS_MULTIPLIER / 100)) * (1 - currentRigOption.structure_bonus / 100)) * 100).toFixed(2);
-    structureBonusInput.value = structure_bonus;
+    const structure_bonus = ((1 - (1 - (currentRigOption.rig_bonus * SYSTEM_BONUS_MULTIPLIER / 100)) * (1 - currentRigOption.structure_bonus / 100)) * 100).toFixed(4);
+    structureBonusInput.setAttribute("accurate-structure-bonus",structure_bonus)
+    structureBonusInput.value = structure_bonus.toFixed(3);
 }
 
 
