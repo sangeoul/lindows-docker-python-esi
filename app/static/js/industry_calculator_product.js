464ef6baedab5ef6a45e57ef681865fc0ad1c3dd
Lindows <sangeoul@naver.com>
2025-01-24 20:42:48 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -68,7 +68,7 @@ class Product {
         this.structure_bonus = 0;
 
         this.table_pannel=document.createElement("table");
-        getMarketPrices();
+        this.getMarketPrices();
         this.makeTable();
     }
 
