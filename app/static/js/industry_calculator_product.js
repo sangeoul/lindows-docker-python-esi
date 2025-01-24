0a55c7b6e508e5844ef8362e4b150063578f5ba9
Lindows <sangeoul@naver.com>
2025-01-24 20:26:48 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -360,6 +360,7 @@ class Product {
             this.setMaterials();
         }
         const tableNextLevel=document.querySelector("#product-pannel-lv"+(this.level+1).toString());
+        console.log("#product-pannel-lv"+(this.level+1).toString() + " : " + typeof tableNextLevel)
         tableNextLevel.innerHTML="";
         this.sortMaterials();
         this.updateTable();
