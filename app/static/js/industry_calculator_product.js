657f5af344e577f215271bb4db5b94b4a093085f
Lindows <sangeoul@naver.com>
2025-01-25 19:37:11 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -348,13 +348,9 @@ class Product {
         this.table_pannel.appendChild(row1);
         this.table_pannel.appendChild(row2);
 
-
-        console.log("!!DEBUG : #product-pannel-lv"+this.manufacturing_level+" pannel add. "+this.itemname);
         const manufacturing_board=document.querySelector("#product-pannel-lv"+this.manufacturing_level);
         
         manufacturing_board.appendChild(this.table_pannel);
-        console.log(manufacturing_board);
-        console.log(this.table_pannel);
         this.showPannel();
 
     }
