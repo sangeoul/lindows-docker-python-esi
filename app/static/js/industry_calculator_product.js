1c7cc5135eacef64f23aeb10e86b123d595ecfb2
Lindows <sangeoul@naver.com>
2025-01-25 20:08:59 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -460,14 +460,14 @@ class Product {
             return;
         }
         this.table_pannel.classList.toggle("hidden-data",!v);
-        /*
+        
         if(this.visibility){
             console.log("Show '"+this.itemname+"' Pannel.");
         }
         if(!this.visibility){
             console.log("Hide '"+this.itemname+"' Pannel.");
         }
-            */
+            
     }
 }
 
