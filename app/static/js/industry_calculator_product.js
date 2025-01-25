466825f0629eb4db673aacd5a09764295d6b5396
Lindows <sangeoul@naver.com>
2025-01-25 20:12:28 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -442,6 +442,7 @@ class Product {
     async selectPannel(){
 
         if(this.manufacturing_level==0){
+            this.openPriceTable(true);
             return;
         }
         for (const materials of this.product_node.materials){
