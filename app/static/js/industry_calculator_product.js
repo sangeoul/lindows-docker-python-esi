9dcf60cf54c2413ff77a04e9f2c84f37c20f837d
Lindows <sangeoul@naver.com>
2025-01-26 23:04:04 +0000

    asdf

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -201,7 +201,8 @@ class Product {
             const jobcost=getJobCost(this.eiv,index,structureBonus,tax,(blueprintdata.industry_type==INDUSTRY_TYPE_NO_DATA?blueprintdata.q:1));
             console.log("!!DEBUG:"+this.itemname+"/ eiv: " +this.eiv + " / index : "+index+ " / structureBonus: "+structureBonus+" / tax:"+tax + " / units per run : "+(blueprintdata.industry_type==INDUSTRY_TYPE_NO_DATA?blueprintdata.q:1));
             console.log(blueprintdata);
-            console.log(blueprintdata.q);
+            console.log(jobcost + " x " + blueprintdata.q);
+            console.log(total + " / " + this.getQuantity());
             this.costprice = (total/this.getQuantity())+jobcost;
         }
         this.updatePanel();
