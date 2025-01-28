872de0b8062770de53654844610bd593f41d115b
Lindows <sangeoul@naver.com>
2025-01-28 13:59:19 +0000

    asdf

--- app/static/js/industry_calculator.js
+++ app/static/js/industry_calculator.js
@@ -95,6 +95,7 @@ function setCookie(name, value, days) {
     d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
     const expires = "expires=" + d.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    console.log("!!DEBUG : cookie saved");
 }
 
 // Function to get a cookie
@@ -104,7 +105,10 @@ function getCookie(name) {
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        if (c.indexOf(nameEQ) == 0) {
+            console.log("!!DEBUG : cookie loaded : "+c);
+            return c.substring(nameEQ.length, c.length);
+        }
     }
     return null;
 }
