108654be8f78baf32802cdaa12657cd43ac88687
Lindows <sangeoul@naver.com>
2025-01-31 11:08:25 +0000

    update

--- app/app.py
+++ app/app.py
@@ -17,7 +17,8 @@ csp = {
     'default-src': "'self'",
     'img-src': ["'self'", "https://images.evetech.net","https://lindows.kr:8009"],
     'connect-src': ["'self'", "https://esi.evetech.net","https://lindows.kr:8009"],
-    'style-src': ["'self'", "https://fonts.googleapis.com"]  # Add Google Fonts here
+    'style-src': ["'self'", "https://fonts.googleapis.com"],  # Add Google Fonts here
+    'font-src': ["'self'", "https://fonts.gstatic.com"]  # Add Google Fonts source for fonts
 }
 # Apply Talisman with the CSP policy
 Talisman(app,content_security_policy=csp)
