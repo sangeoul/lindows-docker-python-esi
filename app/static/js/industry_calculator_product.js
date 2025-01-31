780438a84020c347d6cf1eeedbb14a85d81749a7
Lindows <sangeoul@naver.com>
2025-01-31 00:44:22 +0000

    update

--- app/static/js/industry_calculator_product.js
+++ app/static/js/industry_calculator_product.js
@@ -1472,7 +1472,7 @@ async function calcMaterialBreakdown(breakdownFuelblocks=false) {
         console.log("!!DEBUG materialList_for_unit_calculating[1]");
         console.log(materialList_for_unit_calculating[1]);
         console.log("!!DEBUG materialList_for_unit_calculating[1][11295][20347][0]");
-        console.log(materialList_for_unit_calculating[1][11295][20347][0]);       
+        console.log(materialList_for_unit_calculating[1]["11295"]["20347"][0]);       
 
 
         product_array.forEach(p=>{
