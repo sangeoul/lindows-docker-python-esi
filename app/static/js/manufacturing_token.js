5315adda18a4d3468d8d2a3fc58a5d2ee1f01b4e
Lindows <sangeoul@naver.com>
2025-02-01 10:19:47 +0000

    update

--- app/static/js/manufacturing_token.js
+++ app/static/js/manufacturing_token.js
@@ -1,21 +1,10 @@
-
-const button_active=document.querySelector('#button-copy-active-token');
-
-if(button_active){
-    button_active.addEventListener('click',()=>{
-        const active_token=document.querySelector('#hidden-active-token').value;
-        navigator.clipboard.writeText(active_token);
-    }
-    );
+function copyActiveToken(){
+    const active_token=document.querySelector('#hidden-active-token').value;
+    navigator.clipboard.writeText(active_token);
 }
 
 
-const button_refresh=document.querySelector('#button-copy-refresh-token');
-
-if(button_refresh){
-    button_refresh.addEventListener('click',()=>{
-        const refresh_token=document.querySelector('#hidden-refresh-token').value;
-        navigator.clipboard.writeText(refresh_token);
-    }
-    );
+function copyRefreshToken(){
+    const refresh_token=document.querySelector('#hidden-refresh-token').value;
+    navigator.clipboard.writeText(refresh_token);
 }
