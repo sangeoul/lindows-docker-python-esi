4e739677531a090493e1e2a408a6d052d856cf63
Lindows <sangeoul@naver.com>
2025-02-01 10:25:05 +0000

    update

--- app/static/js/manufacturing_token.js
+++ app/static/js/manufacturing_token.js
@@ -1,10 +1,18 @@
 function copyActiveToken(){
     const active_token=document.querySelector('#hidden-active-token').value;
-    navigator.clipboard.writeText(active_token);
+    navigator.clipboard.writeText(active_token).then(function() { 
+        console.log("Copied.");
+    }).catch(function(e){
+        console.error(`Failed to copy : ${e}`);
+    });
 }
 
 
 function copyRefreshToken(){
     const refresh_token=document.querySelector('#hidden-refresh-token').value;
-    navigator.clipboard.writeText(refresh_token);
+    navigator.clipboard.writeText(refresh_token).then(function() { 
+        console.log("Copied.");
+    }).catch(function(e){
+        console.error(`Failed to copy : ${e}`);
+    });
 }
