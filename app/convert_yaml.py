4552653651283666f6b560bec9d18b8535aea933
Lindows <sangeoul@naver.com>
2025-02-02 12:36:55 +0000

    update

--- app/convert_yaml.py
+++ app/convert_yaml.py
@@ -46,6 +46,8 @@ def main(yaml_file, modules_group, conn):
                     (output_id, output_amount, type_id, 1, 1, type_id)
                 )
             print(f"{type_info['name_en']} has been loaded",flush=True)
+        else
+            print(f"{type_info['name_en']} is not module",flush=True)
 
     with conn.cursor() as cursor:
         save_to_db(module_info_list, conn, cursor)
