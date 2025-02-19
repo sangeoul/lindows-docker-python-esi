2cc1ef6aad18295def2615cfb8bfee4d50971a70
Lindows <sangeoul@naver.com>
2025-02-19 01:53:56 +0000

    update

--- app/static/js/buyback_history.js
+++ app/static/js/buyback_history.js
@@ -4,7 +4,7 @@ document.querySelectorAll(".tab-link").forEach(button => {
     button.addEventListener("click", (e) => {
         // Remove 'active' class from all tabs and tab links
         document.querySelectorAll(".tab-link").forEach(link => link.classList.remove("active"));
-        document.querySelectorAll(".tab-content").forEach(content => content.classList.remove("active"));
+        document.querySelectorAll(".tab").forEach(content => content.classList.remove("active"));
 
         // Add 'active' class to the clicked tab and corresponding tab content
         e.target.classList.add("active");
