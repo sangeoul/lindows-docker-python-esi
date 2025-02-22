8a74d44c3daf044aefaf380e1fb3a5a57216ad89
Lindows <sangeoul@naver.com>
2025-02-22 20:40:30 +0000

    update

--- app/convert_yaml.py
+++ app/convert_yaml.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 import os
 import datetime
 from psycopg2.extras import execute_values
@@ -11,6 +12,11 @@ CHUNK_SIZE = 400
 def read_yaml(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
+
+# Function to read JSON file
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 # Function to save data to database
 def save_to_db(data, conn, cursor):
@@ -26,7 +32,7 @@ def save_to_db(data, conn, cursor):
         DO UPDATE SET output_amount = excluded.output_amount
         """
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f"[{timestamp}]Inserting chunk of size: {len(data)}", flush=True)  # Debugging: Print chunk size
+        print(f"[{timestamp}] Inserting chunk of size: {len(data)}", flush=True)  # Debugging: Print chunk size
         execute_values(cursor, query, data)
         affected_rows = cursor.rowcount  # Check affected rows
         print(f"{affected_rows} rows affected in this chunk.", flush=True)  # Debugging: Print affected rows for this chunk
@@ -37,9 +43,10 @@ def save_to_db(data, conn, cursor):
         conn.rollback()  # Rollback the transaction in case of error
 
 # Main function
-def main(yaml_file, modules_group, conn):
-    data = read_yaml(yaml_file)
-    
+def main(yaml_material, json_bp, searching_group, conn):
+    data = read_yaml(yaml_material)
+    bpdata = read_json(json_bp)
+
     module_info_list = []
     total_items = 0  # Keep track of total items processed
 
@@ -48,14 +55,17 @@ def main(yaml_file, modules_group, conn):
         print(f"type_id: {type_id}", flush=True)
         type_info_response = get_type_info(type_id, str(type_id))
         type_info = type_info_response.get_json()
-        
-        if type_info.get('group_id') in modules_group:
+
+        if type_info.get('group_id') in searching_group:
             for material in details['materials']:
                 output_id = material['materialTypeID']
                 output_amount = material['quantity']
+                manufacturing_quantity = bpdata[str(type_id)]['q']  # Get data from JSON
+
                 module_info_list.append(
-                    (output_id, output_amount, type_id, 1, 1, type_id)
+                    (output_id, output_amount, type_id, manufacturing_quantity, 1, type_id)  # = (output_id, output_amount, input_id, input_amount, industry_type, recipe_id)
                 )
+
                 if len(module_info_list) >= CHUNK_SIZE:
                     with conn.cursor() as cursor:
                         save_to_db(module_info_list, conn, cursor)
@@ -64,38 +74,31 @@ def main(yaml_file, modules_group, conn):
             print(f"{type_info.get('name_en')} has been loaded", flush=True)
         else:
             print(f"{type_info.get('name_en')} is not module", flush=True)
-    
+
     # Save any remaining items in the list
     if module_info_list:
         with conn.cursor() as cursor:
             save_to_db(module_info_list, conn, cursor)
-    
+
     total_items += len(module_info_list)  # Update total items processed
     print(f"Total items processed: {total_items}", flush=True)  # Debugging: Print total count of items processed
 
 if __name__ == "__main__":
     # YAML file path
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    yaml_file = os.path.join(base_dir, 'static', 'typeMaterials.yaml')
+    yaml_material = os.path.join(base_dir, 'static', 'typeMaterials.yaml')
+    json_bp = os.path.join(base_dir, 'static', 'manufacturings.json')
 
     # Define the modules_group as a list
-    modules_group = [
-        38, 39, 40, 41, 43, 46, 47, 48, 49, 52, 53, 54, 55, 56, 57, 59, 60, 61, 62, 63, 65, 67, 68, 71, 72, 74, 76, 
-        77, 78, 80, 82, 96, 98, 201, 202, 203, 205, 208, 209, 210, 211, 212, 213, 225, 285, 289, 290, 291, 295, 302, 
-        308, 309, 315, 316, 317, 321, 325, 326, 328, 329, 330, 338, 339, 341, 353, 357, 367, 378, 379, 405, 406, 407, 
-        464, 472, 475, 481, 483, 499, 501, 506, 507, 508, 509, 510, 511, 512, 514, 515, 518, 524, 538, 546, 585, 586, 
-        588, 589, 590, 638, 642, 644, 645, 646, 647, 650, 658, 660, 737, 753, 762, 763, 764, 765, 766, 767, 768, 769, 
-        770, 771, 773, 774, 775, 776, 777, 778, 779, 781, 782, 786, 815, 842, 862, 878, 896, 899, 901, 904, 905, 1122, 
-        1150, 1154, 1156, 1189, 1199, 1223, 1226, 1232, 1233, 1234, 1238, 1245, 1289, 1292, 1299, 1306, 1308, 1313, 
-        1395, 1396, 1533, 1672, 1673, 1674, 1697, 1698, 1699, 1700, 1706, 1770, 1815, 1894, 1969, 1986, 1988, 2003, 
-        2004, 2008, 2013, 2018, 4060, 4067, 4117, 4127, 4138, 4174, 4184, 4769, 4807
-    ]
+    ammo_group = [
+    83,85,86,87,88,89,90,92,372,373,374,375,376,377,384,385,386,387,394,395,396,425,476,479,482,492,497,498,500,548,648,653,654,655,656,657,663,772,863,864,892,907,908,909,910,911,916,972,1010,1019,1153,1158,1400,1546,1547,1548,1549,1550,1551,1559,1569,1677,1678,1701,1702,1769,1771,1772,1773,1774,1976,1987,1989,4061,4062,4088,4186,4808
+  ]
 
     # Database connection
     conn = connect_to_db()
 
     # Run the main function
-    main(yaml_file, modules_group, conn)
+    main(yaml_material, json_bp, ammo_group, conn)
 
     # Close the database connection
     conn.close()
